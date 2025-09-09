0e23f77efb3c7c4593f59e0983a510a8d7cbea95
Judith Rodriguez <judiths.r91@gmail.com>
2025-09-09 17:37:08 -0400

    a quick start bash script to generate the manuscripts simulation

---
+++ helper_scripts/simulate/simulation_job.sh
@@ -0,0 +1,20 @@
+# Random sequence 5,000 nucleotides in length and a mutation rate (p) of 0.01
+python random_selection_simulation.py --len 5000 --prate 0.01 --wd ../
+# Change p-rate to 0.1
+python random_selection_simulation.py --len 5000 --prate 0.1 --wd ../
+# Change p-rate to 0.001
+python random_selection_simulation.py --len 5000 --prate 0.001 --wd ../
+
+# Random sequence 10,000 nucleotides in length and a mutation rate (p) of 0.01
+python random_selection_simulation.py --len 10002 --prate 0.01 --wd ../
+# Change p-rate to 0.1
+python random_selection_simulation.py --len 10002 --prate 0.1 --wd ../
+# Change p-rate to 0.001
+python random_selection_simulation.py --len 10002 --prate 0.001 --wd ../
+
+# Random sequence 20,000 nucleotides in length and a mutation rate (p) of 0.01
+python random_selection_simulation.py --len 20001 --prate 0.01 --wd ../
+# Change p-rate to 0.1
+python random_selection_simulation.py --len 20001 --prate 0.1 --wd ../
+# Change p-rate to 0.001
+python random_selection_simulation.py --len 20001 --prate 0.001 --wd ../
