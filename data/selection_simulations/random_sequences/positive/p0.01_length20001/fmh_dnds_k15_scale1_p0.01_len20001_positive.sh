a3036c270ff540afc5ee46a1abefe3d06d9c783e
Judith Rodriguez <judiths.r91@gmail.com>
2025-09-10 12:57:54 -0400

    scripts to estimate fmh dnds values for current figure 2a

---
+++ data/selection_simulations/random_sequences/positive/p0.01_length20001/fmh_dnds_k15_scale1_p0.01_len20001_positive.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eoux pipefail
+
+#Parameters
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_practical_considerations/lengths/20001/positive/k15
+k='15'
+scaled=1
+translate_cds='no' #indicate with yes or no
+m="sngl" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
+c=1000
+t=0
+
+#Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
+dna=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_practical_considerations/lengths/20001/positive_selection_queries_20001_0.01.fna
+protein=/data/jzr5814/sourmash_dnds_estimation/tests/results/dnds_practical_considerations/lengths/20001/positive_selection_translated_queries_20001_0.01.faa
+out="prate_0.01"
+
+#Run FMH Omega
+python3 /data/jzr5814/repositories/dnds-using-fmh/src/script_fmh_omega.py --dna_fasta ${dna} --protein_fasta ${protein} --scaled_input ${scaled} --ksize ${k} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} --cores ${c} --threshold ${t} 
