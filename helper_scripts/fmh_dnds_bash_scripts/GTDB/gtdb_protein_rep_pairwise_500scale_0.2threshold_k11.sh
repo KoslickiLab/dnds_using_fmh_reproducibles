3757976347815c96746f40a0f547738bca2d3f2b
Judith Rodriguez <judiths.r91@gmail.com>
2025-09-11 14:17:33 -0400

    scripts to obtain fmh dnds estimations from GTDB added

---
+++ helper_scripts/fmh_dnds_bash_scripts/GTDB/gtdb_protein_rep_pairwise_500scale_0.2threshold_k11.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eoux pipefail
+
+#Parameters
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/results/genomic_dnds/gtdb_protein_rep_pairwise_500scale_0.2threshold_k11
+k='11'
+scaled=500
+translate_cds='no' #indicate with yes or no
+m="bwpair" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
+c=1000
+t=0.2
+
+#Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
+cds_input_list=${wd}/dataset.csv
+out=demo_branchwater
+
+#Run FMH Omega
+python3 /data/jzr5814/repositories/dnds-using-fmh/src/script_fmh_omega.py --fasta_input_list ${cds_input_list} --scaled_input ${scaled} --ksize ${k} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} --cores ${c} --threshold ${t} 
