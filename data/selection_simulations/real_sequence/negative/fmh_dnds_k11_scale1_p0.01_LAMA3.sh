ff674cc2a4424811b9c5973af6df3da480187843
Judith Rodriguez <judiths.r91@gmail.com>
2025-09-11 13:51:10 -0400

    added bash scripts to obtain fmh dnds of lama3 selection simulation

---
+++ data/selection_simulations/real_sequence/negative/fmh_dnds_k11_scale1_p0.01_LAMA3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eoux pipefail
+
+#Parameters
+wd=/data/jzr5814/sourmash_dnds_estimation/tests/test/real_sequence_selection_mutation_0.01/negative/fmh_k11
+k='11'
+scaled=1
+translate_cds='no' #indicate with yes or no
+m="sngl" #branchwater mode, single fasta file use sourmash sketch, multiple fasta files to use sourmash sketch
+c=1
+
+#Note that if using branchwater mode, makesure working directory is at the beginning of filename. THIS IS NOT REQUIERED FOR OTHER MODES
+#cds_input_list=${wd}/dataset.csv
+
+#using sngl mode
+dna_filename=${wd}/../negative_selection_queries_0.01_K06240.fna
+protein_filename=${wd}/../negative_selection_translated_queries_0.01_K06240.faa
+
+#outname
+out=K06240_negative_0.01
+
+#Run FMH Omega
+python3 /data/jzr5814/repositories/dnds-using-fmh/src/script_fmh_omega.py --dna_fasta ${dna_filename} --protein_fasta ${protein_filename} --scaled_input ${scaled} --ksize ${k} --mode ${m} --translate ${translate_cds} --outname ${out} --directory ${wd} --cores ${c} 
+
+
+
